0c9aef39956e6c2a3816fe337bdc63d48d6f7e21
TheDarkBug <adrianoliviero23@gmail.com>
2023-05-20 00:33:30 +0200

    Updated sleep duration slider selector

--- lib/calculate.dart
+++ lib/calculate.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class CalculatePage extends StatefulWidget {
   const CalculatePage({
@@ -12,8 +13,14 @@ class CalculatePage extends StatefulWidget {
 class CalculatePageState extends State<CalculatePage> {
   TimeOfDay _startSleepTime = TimeOfDay.now();
   late TimeOfDay _wakeUpTime;
-  Duration _sleepAmount = const Duration(hours: 7, minutes: 30);
-  Duration _cycleDuration = const Duration(minutes: 90);
+  late Duration _sleepAmount;
+  final Duration _cycleDuration = const Duration(minutes: 90);
+  int _cycleAmount = 5;
+
+  final defaults = const {
+    "cycleDuration": Duration(minutes: 90),
+    "cycleAmount": 5,
+  };
 
   TimeOfDay _addTime(TimeOfDay time, Duration duration) {
     int newMinute = time.minute + duration.inMinutes - (duration.inHours * 60);
@@ -28,14 +35,21 @@ class CalculatePageState extends State<CalculatePage> {
     return TimeOfDay(hour: newHour, minute: newMinute);
   }
 
-  TimeOfDay _subtractTime(TimeOfDay time, Duration duration) {
+  TimeOfDay _subTime(TimeOfDay time, Duration duration) {
     return _addTime(
         time, Duration(hours: -duration.inHours, minutes: -duration.inMinutes));
+  }
+
+  double durationToHours(Duration duration) {
+    double hours = duration.inHours.toDouble();
+    double minutes = duration.inMinutes.toDouble() - (hours * 60);
+    return hours + minutes / 60;
   }
 
   @override
   void initState() {
     super.initState();
+    _sleepAmount = Duration(minutes: _cycleDuration.inMinutes * _cycleAmount);
     _wakeUpTime = _addTime(TimeOfDay.now(), _sleepAmount);
   }
 
@@ -82,7 +96,7 @@ class CalculatePageState extends State<CalculatePage> {
               ).then((value) {
                 setState(() {
                   _wakeUpTime = value!;
-                  _startSleepTime = _subtractTime(now, _sleepAmount);
+                  _startSleepTime = _subTime(now, _sleepAmount);
                   _sleepAmount = const Duration(hours: 7);
                 });
               });
@@ -90,29 +104,71 @@ class CalculatePageState extends State<CalculatePage> {
             child: Text("Wake up at: ${_wakeUpTime.format(context)}"),
           ),
           const SizedBox(height: 20),
+          const Divider(height: 20),
+          const Text("Sleep Amount", style: TextStyle(fontSize: 20)),
           Row(
             children: [
-              const Text("Sleep Amount: "),
-              Slider(
-                value: _sleepAmount.inHours.toDouble(),
-                min: 0,
-                max: 24,
-                divisions: 24 ~/ (_cycleDuration.inMinutes / 60),
-                onChanged: (double value) {
+              IconButton(
+                onPressed: () {
                   setState(() {
-                    _sleepAmount = Duration(
-                      hours: value.toInt(),
-                      minutes: ((value % 1) * 60).toInt(),
-                    );
-                    _wakeUpTime = _addTime(_startSleepTime, _sleepAmount);
+                    int newAmount =
+                        _sleepAmount.inMinutes - _cycleDuration.inMinutes;
+                    if (newAmount >= durationToHours(_cycleDuration)) {
+                      _sleepAmount = Duration(minutes: newAmount);
+                    }
                   });
                 },
+                icon: const Icon(Icons.remove),
+              ),
+              Expanded(
+                child: Slider(
+                  value: durationToHours(_sleepAmount),
+                  min: durationToHours(_cycleDuration),
+                  max: 24,
+                  divisions: (24 - durationToHours(_cycleDuration)) ~/
+                      (_cycleDuration.inMinutes / 60),
+                  onChanged: (double value) {
+                    setState(() {
+                      _sleepAmount = Duration(
+                        hours: value.toInt(),
+                        minutes: ((value % 1) * 60).toInt(),
+                      );
+                      _cycleAmount =
+                          _sleepAmount.inMinutes ~/ _cycleDuration.inMinutes;
+                      _wakeUpTime = _addTime(_startSleepTime, _sleepAmount);
+                    });
+                  },
+                ),
               ),
               Center(
                 child: Text(
                     "${_sleepAmount.inHours}:${(_sleepAmount.inMinutes - (_sleepAmount.inHours * 60)).toString().padLeft(2, '0')}"),
               ),
+              IconButton(
+                onPressed: () {
+                  setState(() {
+                    int newAmount =
+                        _sleepAmount.inMinutes + _cycleDuration.inMinutes;
+                    if (newAmount <= 24 * 60) {
+                      _sleepAmount = Duration(minutes: newAmount);
+                    }
+                  });
+                },
+                icon: const Icon(Icons.add),
+              ),
             ],
+          ),
+          const Divider(height: 20),
+          const Text("Sleep Cycle Information", style: TextStyle(fontSize: 30)),
+          const SizedBox(height: 20),
+          Text(
+            "Duration: ${_cycleDuration.inMinutes} minutes",
+            style: const TextStyle(fontSize: 20),
+          ),
+          const SizedBox(height: 20),
+          Text(
+            "Cycle Amount: $_cycleAmount",
+            style: const TextStyle(fontSize: 20),
           ),
         ],
       ),
